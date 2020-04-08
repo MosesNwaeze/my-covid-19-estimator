f82db8796d1d3bf89776155d91ce09debb41e646
Moses Nwaeze <ogbonnasmart@gmail.com>
2020-04-08 20:19:28 +0100

    commting execercise 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,21 @@
-const covid19ImpactEstimator = (data) => ({
-  data,
-  impact: {
-    currentlyInfected: data.reportedCases * 10,
-    infectionsByRequestedTime: this.currentlyInfected * 1024
-  },
-  severeImpact: {
-    currentlyInfected: data.reportedCases * 50,
-    infectionsByRequestedTime: this.currentlyInfected * 1024
-  }
-});
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: input.reportedCases * 10,
+      get infectionsByRequestedTime() {
+        return this.currentlyInfected * 1024;
+      }
+    },
+    severeImpact: {
+      currentlyInfected: input.reportedCases * 50,
+      get infectionsByRequestedTime() {
+        return this.currentlyInfected * 1024;
+      }
+    }
+  };
+};
+
 
 export default covid19ImpactEstimator;
