71dfbc494d171d68497cf5a0d597e434ca860fcc
Moses Nwaeze <ogbonnasmart@gmail.com>
2020-04-09 09:27:48 +0100

    challenge 2 staged

--- src/severeImpact.js
+++ src/severeImpact.js
@@ -2,23 +2,7 @@ const severeImpact = (data) => {
   const percentageDailyPopulationIncome = Math.floor(
     data.region.avgDailyIncomePopulation
   );
-
-  const bedIndays = () => {
-    switch (data.periodType) {
-      case 'days': {
-        return Math.trunc(data.totalHospitalBeds * (35 / 100));
-      }
-      case 'weeks': {
-        return Math.trunc(data.totalHospitalBeds * (35 / 100));
-      }
-      case 'months': {
-        return Math.trunc(data.totalHospitalBeds * (35 / 100));
-      }
-      default: {
-        return 0;
-      }
-    }
-  };
+  const bedForCovid19 = data.totalHospitalBeds * (35 / 100);
 
   const durationInDays = () => {
     switch (data.periodType) {
@@ -42,7 +26,7 @@ const severeImpact = (data) => {
   const currentlyInfected = data.reportedCases * 50;
   const infectionsByRequestedTime = currentlyInfected * durationInDays();
   const severeCasesByRequestedTime = infectionsByRequestedTime * (15 / 100);
-  const hospitalBedsByRequestedTime = bedIndays() - severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime = Math.trunc(bedForCovid19 - severeCasesByRequestedTime);
   const casesForICUByRequestedTime = infectionsByRequestedTime * (5 / 100);
   const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * (2 / 100);
   const dollarsInFlight = percentageDailyPopulationIncome
