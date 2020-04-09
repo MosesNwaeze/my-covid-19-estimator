3242d00f152f9c7b2920af6a1adb36c7c8f2c03c
Moses Nwaeze <ogbonnasmart@gmail.com>
2020-04-09 07:05:10 +0100

    committing challenges

--- src/estimator.js
+++ src/estimator.js
@@ -1,80 +1,10 @@
-/* eslint-disable consistent-return */
-const covid19ImpactEstimator = (data) => {
-  const availableHospitalBeds = data.totalHospitalBeds * (35 / 100);
-  const percentageDailyPopulationIncome = data.region.avgDailyIncomePopulation;
-  const durationInDays = () => {
-    switch (data.periodType) {
-      case 'days': {
-        return 2 ** Math.floor(data.timeToElapse / 3);
-      }
-      case 'weeks': {
-        const days = data.timeToElapse * 7;
-        return 2 ** Math.floor(days / 3);
-      }
-      case 'months': {
-        const days = data.timeToElapse * 30;
-        return 2 ** Math.floor(days / 3);
-      }
-      default: {
-        return 0;
-      }
-    }
-  };
-  return {
-    data,
-    impact: {
-      currentlyInfected: data.reportedCases * 10,
-      get infectionsByRequestedTime() {
-        return this.currentlyInfected * durationInDays;
-      },
-      get severeCasesByRequestedTime() {
-        return this.infectionsByRequestedTime * (15 / 100);
-      },
-      get hospitalBedsByRequestedTime() {
-        return availableHospitalBeds - this.severeCasesByRequestedTime;
-      },
-      get casesForICUByRequestedTime() {
-        return this.infectionsByRequestedTime * (5 / 100);
-      },
-      get casesForVentilatorsByRequestedTime() {
-        return this.infectionsByRequestedTime * (2 / 100);
-      },
-      get dollarsInFlight() {
-        return (
-          percentageDailyPopulationIncome
-          * this.infectionsByRequestedTime
-          * data.region.avgDailyIncomeInUSD
-          * 30
-        );
-      }
-    },
-    severeImpact: {
-      currentlyInfected: data.reportedCases * 50,
-      get infectionsByRequestedTime() {
-        return this.currentlyInfected * durationInDays;
-      },
-      get severeCasesByRequestedTime() {
-        return this.infectionsByRequestedTime * (15 / 100);
-      },
-      get hospitalBedsByRequestedTime() {
-        return availableHospitalBeds - this.severeCasesByRequestedTime;
-      },
-      get casesForICUByRequestedTime() {
-        return this.infectionsByRequestedTime * (5 / 100);
-      },
-      get casesForVentilatorsByRequestedTime() {
-        return this.infectionsByRequestedTime * (2 / 100);
-      },
-      get dollarsInFlight() {
-        return (
-          percentageDailyPopulationIncome
-          * this.infectionsByRequestedTime
-          * data.region.avgDailyIncomeInUSD
-          * 30
-        );
-      }
-    }
-  };
-};
+import impact from './impact';
+import severeImpact from './severeImpact';
+
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: impact(data),
+  severeImpact: severeImpact(data)
+});
 
 export default covid19ImpactEstimator;
---
+++ src/impact.js
@@ -0,0 +1,44 @@
+const impact = (data) => {
+  const availableHospitalBeds = Math.floor(data.totalHospitalBeds * (35 / 100));
+  const percentageDailyPopulationIncome = Math.floor(data.region.avgDailyIncomePopulation);
+
+  const durationInDays = () => {
+    switch (data.periodType) {
+      case 'days': {
+        return 2 ** Math.floor(data.timeToElapse / 3);
+      }
+      case 'weeks': {
+        const days = data.timeToElapse * 7;
+        return 2 ** Math.floor(days / 3);
+      }
+      case 'months': {
+        const days = data.timeToElapse * 30;
+        return 2 ** Math.floor(days / 3);
+      }
+      default: {
+        return 0;
+      }
+    }
+  };
+  const currentlyInfected = data.reportedCases * 10;
+  const infectionsByRequestedTime = currentlyInfected * durationInDays();
+  const severeCasesByRequestedTime = infectionsByRequestedTime * (15 / 100);
+  const hospitalBedsByRequestedTime = availableHospitalBeds - severeCasesByRequestedTime;
+  const casesForICUByRequestedTime = infectionsByRequestedTime * (5 / 100);
+  const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * (2 / 100);
+  const dollarsInFlight = percentageDailyPopulationIncome
+  * infectionsByRequestedTime
+  * data.region.avgDailyIncomeInUSD
+  * 30;
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
+
+export default impact;
