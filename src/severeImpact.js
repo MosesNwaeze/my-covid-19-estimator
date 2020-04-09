090a65c58a449653e6cfff786949a558727005ec
Moses Nwaeze <ogbonnasmart@gmail.com>
2020-04-09 22:47:47 +0100

    challenge 3 staging-031

--- src/severeImpact.js
+++ src/severeImpact.js
@@ -42,8 +42,8 @@ const severeImpact = (data) => {
   const infectionsByRequestedTime = currentlyInfected * durationInDays();
   const severeCasesByRequestedTime = infectionsByRequestedTime * (15 / 100);
   const hospitalBedsByRequestedTime = Math.trunc(bedForCovid19 - severeCasesByRequestedTime);
-  const casesForICUByRequestedTime = infectionsByRequestedTime * (5 / 100);
-  const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * (2 / 100);
+  const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime * (5 / 100));
+  const casesForVentilatorsByRequestedTime = Math.floor(infectionsByRequestedTime * (2 / 100));
   const dollarsInFlight =
   Math.floor(infectionsByRequestedTime
         * avgDailyIncomePopulation
