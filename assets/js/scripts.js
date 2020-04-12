fef1a1d151588354fd3f2e422b2d2e40e13023de
Moses Nwaeze <ogbonnasmart@gmail.com>
2020-04-12 10:37:57 +0100

    Committing index page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,26 +2,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable camelcase */
 
-function scroll_to_class(element_class, removed_height) {
-  const scroll_to = $(element_class).offset().top - removed_height;
-  if ($(window).scrollTop() !== scroll_to) {
-    $('html, body').stop().animate({ scrollTop: scroll_to }, 0);
-  }
-}
-
-function bar_progress(progress_line_object, direction) {
-  const number_of_steps = progress_line_object.data('number-of-steps');
-  const now_value = progress_line_object.data('now-value');
-  let new_value = 0;
-  if (direction === 'right') {
-    new_value = now_value + 100 / number_of_steps;
-  } else if (direction === 'left') {
-    new_value = now_value - 100 / number_of_steps;
-  }
-  progress_line_object
-    .attr('style', `width: ${new_value}%;`)
-    .data('now-value', new_value);
-}
 
 jQuery(document).ready(() => {
   /*
